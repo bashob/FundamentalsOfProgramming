037bd775f96481e6bfecd07c2f482b16cd1eaf1f
bashob <bashobnandy@gmail.com>
2021-10-26 15:46:36 +0530

    First version

---
+++ src/com/ComputerVlogs/Variables.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Variables {
+    public static void main(String[] args){
+
+        // declaring variables
+        int marks1;
+        int marks2;
+
+        // assigning values and initialization of variables
+        marks1 = 72;
+        marks2 = 85;
+
+        // declaring variables in a single line
+        String student1 = "Aryan";
+        String student2 = "Shabana";
+
+        System.out.println(marks1);
+        System.out.println(student2);
+
+    }
+}
